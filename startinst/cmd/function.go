ef2baf90204506dfa97228daf6c2b5694b8893e0
gotochkin <gleb.otochkin@gmail.com>
2022-03-13 16:47:14 +0000

    gke tests

---
+++ startinst/cmd/function.go
@@ -0,0 +1,63 @@
+/// Package p contains a Pub/Sub Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"context"
+	"fmt"
+	"log"
+	sqladmin "google.golang.org/api/sqladmin/v1beta4"
+)
+
+// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
+// additional information regarding Pub/Sub events.
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+type Parameters struct {
+	Project string `json:"project"`
+	Cluster string `json:"cluster"`
+	Size int64 `json: size`
+}
+// ResizeFunc consumes a Pub/Sub message.
+func ResizeFunc(ctx context.Context, m PubSubMessage) error {
+	var par Parameters 
+	err := json.Unmarshal(m.Data,&par) 
+	if err != nil {
+		log.Println(err)
+	}
+	//log.Println(string(m.Data))
+	log.Println(string(par.Project))
+	log.Println(string(par.Cluster))
+	log.Println(string(par.Size))
+	// Create context
+	containerService, err := container.NewService(ctx)
+	sizeRequest := &container.SetNodePoolSizeRequest {
+		NodeCount: par.Size,
+	}
+	parent := fmt.Sprintf("projects/%s/locations/-", par.Project)
+	listClusters, err := containerService.Projects.Locations.Clusters.List(parent).Do()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(len(listClusters.Clusters))
+	for _, cluster := range listClusters.Clusters {
+		if cluster.Name == par.Cluster {
+			fmt.Println("cluster name:",cluster.Name,
+			            "Node count:", cluster.CurrentNodeCount,
+		    )
+		    for _, nodepool := range cluster.NodePools {
+			    fmt.Println("Node Pool Name:",nodepool.Name)
+				parentsize := fmt.Sprintf("projects/%s/locations/%s/clusters/%s/nodePools/%s", 
+				                          par.Project,cluster.Location,cluster.Name,nodepool.Name)
+				nodesize, err := containerService.Projects.Locations.Clusters.NodePools.SetSize(parentsize,sizeRequest).Do()
+				if err != nil {
+					log.Println(err)
+				}
+				fmt.Println(nodesize)
+		    }
+		}		
+	}
+	fmt.Println("Done")
+	return nil
+}
